6ee17db6ea9f031b52cae338e7f7dfd38306a494
Junsu Park <junsupark94@gmail.com>
2023-07-28 19:10:09 -0700

    Created Draft type with XOR typing in draft-modal-store.ts

--- app/store/drafts-modal-store.ts
+++ app/store/drafts-modal-store.ts
@@ -1,10 +1,27 @@
 import { create } from "zustand";
+import { Media } from "./tweets-store";
 
-type Draft = {
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
+
+interface DraftBasic {
   body: string;
-  replying?: string,
   id: string;
 }
+interface QuoteTweetDraft extends DraftBasic {
+  replying?: never;
+  quoteTweetUrl: string;
+  quoteTweetbody?: string;
+  quoteTweetMedia?: Media[];
+}
+interface ReplyDraft extends DraftBasic {
+  quoteTweetUrl?: never;
+  replying: string;
+  replyBody?: string;
+  replyMedia?: string;
+}
+type Draft = XOR<DraftBasic, XOR<QuoteTweetDraft, ReplyDraft>>;
+
 
 interface DraftsModalStore {
   isVisible: boolean;
@@ -20,10 +37,21 @@ const useDraftsModalStore = create<DraftsModalStore>((set) => ({
   openModal: () => set({ isVisible: true }),
   closeModal: () => set({ isVisible: false }),
   closeSourceModal: null,
-  setSourceModal: (closeModal : any) => set({closeSourceModal: closeModal}),
+  setSourceModal: (closeModal: any) => set({ closeSourceModal: closeModal }),
   drafts: [
-    {body: "Test draft", replying: "junsupark", id: "d1"},
-    {body: "Test draft 2", id: "d2"},
+    {
+      body: "Test draft",
+      replying: "junsupark",
+      id: "d1",
+      replyBody: "Reply body",
+    },
+    {
+      body: "Test draft 2",
+      id: "d2",
+      quoteTweetbody: "Quote Tweet Body",
+      quoteTweetUrl: "https://twitter.com/username/status/tweetid",
+    },
+    { body: "Test", id: "d3" },
   ],
 }));
 
